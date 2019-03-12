8ff4d38bbf2959261d91a3e60c90b2a8f14e2782
Niko <Nirailay@gmail.com>
2019-03-12 14:58:09 +0100

    Populate database

--- DBco.js
+++ DBco.js
@@ -2,17 +2,22 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-const CONNECTION_URL = "mongodb+srv://Me:<Nikoukou1>@cluster0-vv8ab.mongodb.net/test?retryWrites=true";
+
+const CONNECTION_URL = "mongodb+srv://Me:Nikoukou1@cluster0-vv8ab.mongodb.net/test?retryWrites=true";
 const DATABASE_NAME = "film";
 
-var app = Express();
+const imdb = require('./src/imdb');
+const DENZEL_IMDB_ID = 'nm0000243';
 
+
+
+var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 var database, collection;
 
-app.listen(3000, () => {
+app.listen(9292, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
@@ -20,5 +25,27 @@ app.listen(3000, () => {
         database = client.db(DATABASE_NAME);
         collection = database.collection("denzel");
         console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+
+
+/*app.post("/movies/populate", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});*/
+
+app.get("/movies/populate", async (request, response) => {
+  const movies = await imdb(DENZEL_IMDB_ID);
+    
+    collection.insertMany(movies, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
     });
 });
